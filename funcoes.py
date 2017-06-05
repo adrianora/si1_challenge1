6efb16b7454d4ef5198ef35ff2ba2410d86a6292
adrianora <adriano.araujo@ccc.ufcg.edu.br>
2017-06-05 12:09:57 -0300

    Procura menor inteiro

--- funcoes.py
+++ funcoes.py
@@ -4,3 +4,10 @@ def searchMax(array):
         if array[index] > max:
             max = array[index]
     return max
+
+def searchMin(array):
+    min = array[0]
+    for index in range(len(array)):
+        if array[index] < min:
+            min = array[index]
+    return min
